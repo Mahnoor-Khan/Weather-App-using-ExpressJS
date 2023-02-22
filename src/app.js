e6588b1fbd81ea599448a8ce0522c445e220d6f1
Mahnoor Khan <mahnoorza001@gmail.com>
2023-02-23 01:34:37 +0500

    Home and about page

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 const app = express();
 const port = process.env.PORT || 8000;
 
 // public static path
 let staticPath = path.join(__dirname , '../public')
+let template_path = path.join(__dirname , './templates/views')
+let partials_path = path.join(__dirname , './templates/partials') 
 app.use(express.static(staticPath))
+app.set('view engine' , 'hbs')
+app.set('views' , template_path);
+hbs.registerPartials(partials_path)
 
 // rouitng
 app.get('/' , (req, res) => {
-    res.send('New App created')
+    res.render('index')
 });
 app.get('/weather' , (req, res) => {
-    res.send('Welcome to weather page')
+    res.render('weather')
 });
 app.get('/about' , (req, res) => {
-    res.send('Welcome to about page')
+    res.render('about')
 });
 app.get('*' , (req, res) => {
-    res.send('Page not Found , 404 ERROR!')
+    res.render('404ErrorPage') 
 });
 
 app.listen(port , ()=>{
